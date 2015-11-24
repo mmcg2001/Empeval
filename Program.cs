930c3eb17257a4afe406c62a38172997a6ee7496
mmcg2001 <mmcgregor2001@gmail.com>
2015-11-24 10:00:10 -0600

    C# Attemp2

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = true;
+            do
+            {
+                 for(int x = 0 ; x < 10; x++)
+                 {
+                    Console.WriteLine("This is loop number: " + x);
+                    if (x == 5)
+                    {
+                        Console.WriteLine("It's half over");
+                    }
+                 }
+                    Console.WriteLine("Do You Want To Continue, Yes Or No: ");
+                 if (Console.ReadLine().ToUpper().Equals("YES"))
+                 {
+                    cont = true;
+                 }
+                 else
+                 {
+                    cont = false;
+                    Console.WriteLine("Hit Enter To Exit");
+                 }
+            }while(cont);
+
+            Console.ReadLine();
+        }
+    }
+}
